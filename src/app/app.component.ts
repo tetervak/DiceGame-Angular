5c75e2a80d26e9819db323662c6fa51c81c56820
Alex Tetervak <tetervak@sheridancollege.ca>
2025-01-16 10:28:41 -0500

    made 3 dice work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {NgOptimizedImage} from '@angular/common';
+import {RollData} from './roll-data';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +16,30 @@ import {NgOptimizedImage} from '@angular/common';
 })
 export class AppComponent {
 
-  diceValue: number = 6;
+  rollData: RollData | undefined;
 
   private getRandomDiceValue(): number {
     return 1 + Math.floor( 6 * Math.random())
   }
 
+  private getRollData(numberOfDice: number): RollData {
+    const values: number[] = [];
+    let total: number = 0;
+    for(let i = 0; i < numberOfDice; i++){
+      let diceValue: number = this.getRandomDiceValue();
+      total += diceValue;
+      values.push(diceValue);
+    }
+
+    return {
+      numberOfDice: numberOfDice,
+      values: values,
+      total: total
+    }
+  }
+
   onRoll(): void {
-    this.diceValue = this.getRandomDiceValue();
+    this.rollData = this.getRollData(3);
   }
 
   imageSrc(value: number): string {
