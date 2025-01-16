315a099f65b8abf315005af59b23f1fb49d63f13
Alex Tetervak <tetervak@sheridancollege.ca>
2025-01-16 09:18:26 -0500

    added the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {DatePipe} from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    DatePipe
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  today: Date = new Date();
+}
